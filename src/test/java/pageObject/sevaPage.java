4a21075fc34a11e486e351b239b62c93988f64a0
rehanshaikh55 <shaikhrehan1016@gmail.com>
2025-04-02 12:56:25 +0530

    Enhance null analysis settings and update Java compiler preferences

--- src/test/java/pageObject/sevaPage.java
+++ src/test/java/pageObject/sevaPage.java
@@ -1,14 +1,10 @@
 package pageObject;
 
-import static org.testng.Assert.assertEquals;
-
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.asserts.SoftAssert;
-
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 
